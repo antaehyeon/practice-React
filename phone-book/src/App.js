445788967d787b27bbf715961d6ec9b31a85d01e
HYEON(Mac) <antaehyeon@live.co.kr>
2018-10-25 12:25:34 +0900

    Feat: onSubmit Event 를 통한 부모와 데이터 교환 구현
    새로고침 방지를 위한 preventDefault 메서드 적용

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-import PhoneForm from './component/PhoneForm.js';
+import PhoneForm from './component/PhoneForm';
 
 class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
   render() {
     return (
       <div>
-        <PhoneForm/>
+        <PhoneForm
+          onCreate = {this.handleCreate}
+        />
       </div>
     );
   }
--- phone-book/src/component/PhoneForm.js
+++ phone-book/src/component/PhoneForm.js
@@ -11,9 +11,20 @@ class PhoneForm extends Component {
       [e.target.name]: e.target.value
     })
   }
+  handleSubmit = (e) => {
+    // 페이지 리로딩 방지
+    e.preventDefault();
+    // onCreate 메서드로 상태 값(this.state) 전달
+    this.props.onCreate(this.state);
+    // 상태 초기화
+    this.setState({
+      name:'',
+      phone:'',
+    })
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           placeholder="이름"
           value={this.state.name}
@@ -26,7 +37,7 @@ class PhoneForm extends Component {
           onChange={this.handleChange}
           name="phone"
         />
-        <div>{this.state.name} {this.state.phone}</div>
+        <button type="submit">등록</button>
       </form>
     );
   }
