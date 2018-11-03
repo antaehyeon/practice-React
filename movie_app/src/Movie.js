9dcf09026b934ed917f539e4936efc8d68a1c7ea
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-11-03 21:29:54 +0900

    Feat: #6-1 Smart vs Dumb Components

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
-import './Movie.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./Movie.css";
+import PropTypes from "prop-types";
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.isRequired,
-        poster: PropTypes.isRequired,
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component {
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
-    static propTypes = {
-        poster: PropTypes.isRequired,
-    }
-
-    render() {
-        return (
-            <img src={this.props.poster} alt=""/>
-        )
-    }
+function MoviePoster({ poster }) {
+  return <img src={poster} alt="Movie Poster" />;
 }
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
 
 export default Movie;
