f56d420422f6c87415c8493c7449a6d7d5e3d567
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-11-03 23:14:10 +0900

    Feat: #6-3. Async Await in React

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,33 +3,51 @@ import "./App.css";
 import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
   state = {};
 
-  componentWillMount() {
-    console.log("WILL MOUNT");
-  }
-
   componentDidMount() {
-    console.log("DID MOUNT");
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=like_count")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie key={index} title={movie.title} poster={movie.poster} />;
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
     });
     return movies;
   };
 
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
   render() {
-    console.log("RENDER");
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "LOADING :)"}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
