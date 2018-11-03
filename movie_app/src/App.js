daac45eaed9356625b1122881e2d131c3c85bdec
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-11-03 20:54:30 +0900

    Feat: #4-2. Practicing this.setState()

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
-
-const movies = [
-  {
-    title: 'Matrix',
-    poster: 'https://i.imgur.com/ZhmnxQC.png',
-  },
-  {
-    title: 'Full Metal Jacket',
-    poster: 'https://i.imgur.com/ZhmnxQC.png',
-  },
-  {
-    title: 'Oldboy',
-    poster: 'https://i.imgur.com/tSukFdb.jpg',
-  },
-  {
-    title: 'Star Wars',
-    poster: 'https://i.imgur.com/KV4jBox.jpg',
-  },
-];
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
-
   componentWillMount() {
-    console.log('WILL MOUNT');
+    console.log("WILL MOUNT");
   }
 
   componentDidMount() {
-    console.log('DID MOUNT');
+    console.log("DID MOUNT");
     setTimeout(() => {
       // this.state.greeting = "BYE";
       this.setState({
-        greeting: 'BYE',
-      })
+        movies: [
+          ...this.state.movies,
+          {
+            title: "PerfectStranger",
+            poster: "https://newsimg.sedaily.com/2018/09/11/1S4LHMS4H2_1.jpg"
+          }
+        ]
+      });
     }, 5000);
   }
 
   state = {
-    greeting: 'HELLO !',
-  }
+    greeting: "HELLO !",
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://i.imgur.com/ZhmnxQC.png"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://i.imgur.com/ZhmnxQC.png"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://i.imgur.com/tSukFdb.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://i.imgur.com/KV4jBox.jpg"
+      }
+    ]
+  };
 
   render() {
-    console.log('RENDER');
+    console.log("RENDER");
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
-          return <Movie key={index} title={movie.title} poster={movie.poster}/>
+        {this.state.movies.map((movie, index) => {
+          return (
+            <Movie key={index} title={movie.title} poster={movie.poster} />
+          );
         })}
       </div>
     );
